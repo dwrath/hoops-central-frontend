e9c9f84f1baf499fdee6dca7f7a7b05a06380924
Damian Wrather <damian.wrather@gmail.com>
2023-07-21 13:16:36 -0500

    Navbar updates

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 "use client";
-
+import '@styles/navbar.css';
 import React from 'react';
 import Link from 'next/link';
 import Image from "next/image";
@@ -8,71 +8,72 @@ import {signIn, signOut, useSession, getProviders} from "next-auth/react";
 
 const Navbar = () => {
     const isLoggedIn = false;
+
     return(
         <>
-        {!isLoggedIn ? (
-        <div className="container justify-center">
-            <div className="row justify-space-between align-center">
-                <div className="column shrink">
-                    <div className="row">
-                        <div className="column shrink">
-                            <Image src="/assets/images/basketball-logo.svg"
-                                   alt="basketball"
-                                    width={30}
-                                    height={30}
-                                   className="object-contain"
-                            />
-                        </div>
-                        <div className="column">
-                            <h1>Hoops Central</h1>
+        {isLoggedIn ? (
+            <div className="container justify-center nav-bg">
+                <div className="row justify-space-between align-center">
+                    <div className="column shrink">
+                        <div className="row">
+                            <div className="column shrink justify-center">
+                                <Image src="/assets/images/basketball-logo.svg"
+                                       alt="basketball"
+                                       width={30}
+                                       height={30}
+                                       className="object-contain"
+                                />
+                            </div>
+                            <div className="column">
+                                <h1>Hoops Central</h1>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="column shrink">
-                    <nav className="row">
-                        <div className="column"><Link href="/">About</Link></div>
-                        <div className="column"><Link href="/">Contact</Link></div>
-                    </nav>
-                </div>
-                <div className="column shrink">
-                    <div className="row">
-                        <button className="button">Sign In</button>
-                        <button className="button">Sign Up</button>
+                    <div className="column shrink">
+                        <nav className="row">
+                            <div className="column"><Link href="/">About</Link></div>
+                            <div className="column"><Link href="/">Contact</Link></div>
+                        </nav>
+                    </div>
+                    <div className="column shrink">
+                        <div className="row">
+                            <button type="button" className="button">Logout</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         ) : (
-                <div className="container justify-center">
-                    <div className="row justify-space-between align-center">
-                        <div className="column shrink">
-                            <div className="row">
-                                <div className="column shrink">
-                                    <Image src="/assets/images/basketball-logo.svg"
-                                           alt="basketball"
-                                           width={30}
-                                           height={30}
-                                           className="object-contain"
-                                    />
-                                </div>
-                                <div className="column">
-                                    <h1>Hoops Central</h1>
-                                </div>
+            <div className="container justify-center">
+                <div className="row justify-space-between align-center">
+                    <div className="column shrink">
+                        <div className="row">
+                            <div className="column shrink">
+                                <Image src="/assets/images/basketball-logo.svg"
+                                       alt="basketball"
+                                       width={30}
+                                       height={30}
+                                       className="object-contain"
+                                />
                             </div>
-                        </div>
-                        <div className="column shrink">
-                            <nav className="row">
-                                <div className="column"><Link href="/">About</Link></div>
-                                <div className="column"><Link href="/">Contact</Link></div>
-                            </nav>
-                        </div>
-                        <div className="column shrink">
-                            <div className="row">
-                                <button type="button" className="button">Logout</button>
+                            <div className="column">
+                                <h1>Hoops Central</h1>
                             </div>
                         </div>
                     </div>
+                    <div className="column shrink">
+                        <nav className="row">
+                            <div className="column"><Link href="/">About</Link></div>
+                            <div className="column"><Link href="/">Contact</Link></div>
+                        </nav>
+                    </div>
+                    <div className="column shrink">
+                        <div className="row">
+                            <button className="button">Sign In</button>
+                            <button className="button">Sign Up</button>
+                        </div>
+                    </div>
                 </div>
+            </div>
         )}
         </>
     )
